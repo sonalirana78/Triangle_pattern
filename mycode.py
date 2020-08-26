8394686634c843ec3e7b04fe89dcdee258b0282e
sonali rana <sonalirana78@gmail.com>
2020-08-26 10:42:11 +0530

    Initial commit

--- mycode.py
+++ mycode.py
@@ -1,4 +1,8 @@
 for i in range(4):
-    for j in range (4):
+    for j in range (i):
+        print("*",end="")
+    print()
+for i in range(4):
+    for j in range (4-i):
         print("*",end="")
     print()
